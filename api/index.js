b4a4fe40e72c0c9817bec06586550e062f3ebfd5
Icaro Rocha <irpmobilecontato@gmail.com>
2025-08-09 16:19:06 -0300

    Remove API implementation from project

--- api/index.js
+++ api/index.js
@@ -1,14 +1,1 @@
-const express = require('express');
-const app = express();
 
-// Importa rotas
-const diagnostic = require('./routes/diagnostic');
-app.use('/api', diagnostic);
-
-// ...existing code...
-
-// Sobe o servidor na porta 4000 e em todas as interfaces
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`API rodando na porta ${PORT}`);
-});
